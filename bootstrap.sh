a904ecd0b390afa698c1044cc8fe922a21475f12
Darryl Abbate <rootbeersoup@gmail.com>
2018-03-01 18:57:24 -0600

    changed bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,9 +6,7 @@ softwareupdate -i -a
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-rm ~/.bash_profile
-ln -s ~/dotfiles/.bash_profile ~
+brew bundle
 
-function createSymlinks() {
-
-}
+stow bash
+stow vim
