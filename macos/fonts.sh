653403009c41c625b73ae92147f05a250b713ee8
Darryl Abbate <rootbeersoup@gmail.com>
2018-07-03 17:26:32 -0500

    Fix typo

--- macos/fonts.sh
+++ macos/fonts.sh
@@ -5,6 +5,6 @@ brew cask install font-oswald
 brew cask install font-m-plus
 brew cask install font-monda
 brew cask install font-nova-mono
-brew cask install share-tech-mono
+brew cask install font-share-tech-mono
 brew cask install font-source-code-pro
 brew cask install font-source-sans-pro
