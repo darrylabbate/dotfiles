be1b430b81a25c70b34f6395af3d9f5bec625309
Darryl Abbate <rootbeersoup@gmail.com>
2018-07-06 13:50:50 -0500

    [apt.sh] Apache -> Nginx

--- linux/apt.sh
+++ linux/apt.sh
@@ -15,4 +15,4 @@ apt-get install tree
 apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
 
 # Install Apache web server
-apt-get install apache2
+apt-get install nginx
