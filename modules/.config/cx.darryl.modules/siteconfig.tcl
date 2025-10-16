70b4e40dc591c1ab21bee5e2cb7b1cc090b158b9
Darryl Abbate <drl@amazon.com>
2025-10-16 15:29:28 -0700

    Discover `bash-eval` shell option for `source-sh`
    This removes all the ridiculous (source|cache)-cmd logic

--- modules/.config/cx.darryl.modules/siteconfig.tcl
+++ modules/.config/cx.darryl.modules/siteconfig.tcl
@@ -22,43 +22,7 @@ proc putSepLine {} {
     }
 }
 
-proc is-expired {cached_file {ttl 86400}} {
-    return [expr {[clock seconds] - [file mtime $cached_file] > $ttl}]
-}
-
-proc is-newer {file1 file2} {
-    return [expr {[file mtime $file1] > [file mtime $file2]}]
-}
-
-# Cache the output of a command to a file for later use with source-sh
-# This is useful when a module needs to post-process the script output before
-# invoking source-sh
-proc cache-cmd {command {source_file ""}} {
-    # TODO Accommodate namespaced caches
-    set namespace cx.darryl.modules
-    global XDG_CACHE_HOME
-    set cache_file [file join $XDG_CACHE_HOME $namespace "[string map {/ _ " " _} [join $command _]].[module-info shell]"]
-    
-    if {![file exists $cache_file] || 
-        ($source_file ne "" && [file exists $source_file] && [is-newer $source_file $cache_file]) ||
-        [is-expired $cache_file]} {
-        file mkdir [file dirname $cache_file]
-        set fd [open $cache_file w]
-        puts $fd [exec {*}$command]
-        close $fd
-    }
-    return $cache_file
-}
-
-# source-sh the output of `command`
-# This writes the output to a file first, since source-sh requires a file path
-# to the script to be sourced in order to properly reverse the operation upon
-# unloading
-proc source-cmd {command {source_file ""}} {
-    source-sh [module-info shell] [cache-cmd $command $source_file]
-}
-
-set modulefile_extra_cmds {cache-cmd cache-cmd source-cmd source-cmd putSepLine putSepLine}
+set modulefile_extra_cmds {putSepLine putSepLine}
 set modulefile_extra_vars $extra_vars
 set modulerc_extra_vars $extra_vars
 
