1b98ba9a6436fa1e4855100df47b58743a5ebd6d
Darryl Abbate <drl@amazon.com>
2025-12-09 15:58:00 -0800

    Lazy-load spack

--- spack/.config/cx.darryl.bash/profile.d/01-spack.bash
+++ spack/.config/cx.darryl.bash/profile.d/01-spack.bash
@@ -2,11 +2,22 @@ export SPACK_USER_CONFIG_PATH=${XDG_CONFIG_HOME:-$HOME/.config}/spack
 export SPACK_USER_CACHE_PATH=${XDG_CACHE_HOME:-$HOME/.cache}/spack
 
 fsx_rootdir=/fsx/$USER
-spack=~/spack
+_spack_root=${SPACK_ROOT:-$HOME/spack}
 
 [[ -d "$fsx_rootdir" ]] && {
     export SPACK_USER_CACHE_PATH=$fsx_rootdir/cache/spack
-    spack_root=$fsx_rootdir/spack
+    _spack_root=$fsx_rootdir/spack
 }
 
-. $spack/share/spack/setup-env.sh
+_spack_setup="$_spack_root/share/spack/setup-env.sh"
+
+# Lazy-load Spack
+spack() {
+    unset -f spack
+    [[ -r $_spack_setup ]] || {
+        printf 'spack: setup file not found at %s\n' "$_spack_setup" >&2
+        return 127
+    }
+    . "$_spack_setup"
+    spack "$@"
+}
